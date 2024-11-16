70517082d70599d4fc2be832c7a04deeda630e15
Мацюк Никита <matsuk-nikita@rambler.ru>
2024-11-16 16:44:15 +0500

    add wells dto

--- backend/src/wells/dto/create-well.dto.ts
+++ backend/src/wells/dto/create-well.dto.ts
@@ -1,1 +1,39 @@
-export class CreateWellDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsISO8601, IsNotEmpty, IsString, IsInt } from 'class-validator';
+
+export class CreateWellDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  related_well: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsISO8601()
+  date_fact: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  debit: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  ee_consume: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  expenses: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  pump_operating: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsInt()
+  well: number
+}
--- backend/src/wells/dto/update-well.dto.ts
+++ backend/src/wells/dto/update-well.dto.ts
@@ -1,4 +1,39 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateWellDto } from './create-well.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsISO8601, IsNotEmpty, IsString, IsInt } from 'class-validator';
 
-export class UpdateWellDto extends PartialType(CreateWellDto) {}
+export class CreateWellDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  related_well: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsISO8601()
+  date_fact: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  debit: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  ee_consume: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  expenses: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  pump_operating: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsInt()
+  well: number
+}
