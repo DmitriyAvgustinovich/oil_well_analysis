99a4b4ff3070e5d4672b150db24db3d1fabde85b
Мацюк Никита <matsuk-nikita@rambler.ru>
2024-11-17 06:55:04 +0500

    dtos for notifications

--- backend/src/notifications/dto/create-notification.dto.ts
+++ backend/src/notifications/dto/create-notification.dto.ts
@@ -1,1 +1,23 @@
-export class CreateNotificationDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateNotificationDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  type: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  wall_id: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  user_id: number
+}
